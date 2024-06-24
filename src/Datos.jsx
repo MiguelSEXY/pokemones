1b9bd057d348ebcd794943d0d9f60a91333ae96e
Miguel <vasquezsalasmiguelangel@gmail.com>
2024-06-24 13:57:10 -0400

    Editado

--- src/Datos.jsx
+++ src/Datos.jsx
@@ -1,19 +1,14 @@
 function Datos(props){
     return(
-        <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="..." alt="Card image cap"/>
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        </div>
-        // <div className="Contenedor_Pokemon">
-        //     {props.pokemons.map((datospokemons)=>(
-        //     <div className="pokemon">
-        //         <h1>{datospokemons.nombre_pokemon}</h1>
-        //         <img src={datospokemons.img_pokemon} />
-        //     </div>
-        //     ))}
-        // </div>
+       
+         <div className="Contenedor_Pokemon">
+             {props.pokemons.map((datospokemons)=>(
+             <div className="pokemon">
+                 <h1>{datospokemons.nombre_pokemon}</h1>
+                 <img src={datospokemons.img_pokemon} />
+             </div>
+             ))}
+         </div>
     )
 }
 export default Datos;
